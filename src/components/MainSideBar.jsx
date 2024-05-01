9d4e8dd1ff374053b82a793ae4b21007240046ce
MiloCo86 <cpaez@pursuit.org>
2024-05-01 01:10:39 -0400

    MainSideBar initial component created with basic css

---
+++ src/components/MainSideBar.jsx
@@ -0,0 +1,24 @@
+
+import "../styles/MainSideBar.css"
+
+function MainSideBar(){
+
+    return(
+        <div className="side_bar">
+            <h2>Pursuit</h2>
+            <ul>
+                <li><p>All Students</p></li>
+                <li><p>Winter 2026</p></li>
+                <li><p>Fall 2026</p></li>
+                <li><p>Summer 2026</p></li>
+                <li><p>Spring 2026</p></li>
+                <li><p>Winter 2025</p></li>
+                <li><p>Fall 2025</p></li>
+                <li><p>Summer 2025</p></li>
+                <li><p>Spring 2025</p></li>
+            </ul>
+        </div>
+    )
+}
+
+export default MainSideBar;
